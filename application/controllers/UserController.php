f7f5800dda71a4f31d9ad36df8df008743597982
Seung-jae <tmdwo5040@gmail.com>
2022-06-24 14:47:53 +0900

    .

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,16 +1,19 @@
 <?php
 namespace application\controllers;
 
-require_once "application/utils/UrlUtils.php";
+require_once 'application/utils/UrlUtils.php';
 
-class UserController extends Controller {
-    public function signin() {        
-        return "user/signin.php";
+class UserController extends Controller
+{
+    public function signin()
+    {
+        return 'user/signin.php';
     }
 
-    public function signup() {
+    public function signup()
+    {
         $method = getMethod();
-        switch($method) {
+        switch ($method) {
             case _GET:
                 return;
             case _POST:
