7662a1397d9a7f528a7eb6448efa830e2c15f29f
Seung-jae <tmdwo5040@gmail.com>
2022-06-24 14:33:45 +0900

    .

---
+++ application/controllers/Controller.php
@@ -0,0 +1,55 @@
+<?php
+namespace application\controllers;
+
+class Controller {    
+    protected $model;
+    private static $needLoginUrlArr = [
+        "/board/write", 
+        "/board/mod"
+    ];
+
+    public function __construct($action, $model) {    
+        if(!isset($_SESSION)) {
+            session_start();
+        }    
+        $this->model = $model;
+        $view = $this->$action();
+        if(empty($view)) {
+            echo "Controller 에러 발생";
+            exit();
+        }
+
+        if(gettype($view) === "string") {
+            require_once $this->getView($view);             
+        } else if(gettype($view) === "object" || gettype($view) === "array") {
+            header("Content-Type:application/json");
+            echo json_encode($view);
+        }        
+    }
+    private function chkLoginUrl() {
+
+    }
+    
+    protected function addAttribute($key, $val) {
+        $this->$key = $val;
+    }
+
+    protected function getView($view) {
+        if(strpos($view, "redirect:") === 0) {
+            header("Location: " . substr($view, 9));
+            exit();
+        }
+        return _VIEW . $view;
+    }
+
+    protected function flash($name = '', $val = '') {
+        if(!empty($name)) { //공백이 아니면
+            if(!empty($val)) {
+                $_SESSION[$name] = $val;
+            } else if(empty($val) && !empty($_SESSION[$name])) {
+                unset($_SESSION[$name]);
+            }
+        }
+    }
+}
+
---
+++ application/controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+namespace application\controllers;
+
+require_once "application/utils/UrlUtils.php";
+
+class UserController extends Controller {
+    public function signin() {        
+        return "user/signin.php";
+    }
+
+    public function signup() {
+        $method = getMethod();
+        switch($method) {
+            case _GET:
+                return;
+            case _POST:
+                return;
+        }
+    }
+}
