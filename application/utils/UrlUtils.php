3a2705953f594d6e936697a1870be68a2662f309
Seung-jae <tmdwo5040@gmail.com>
2022-06-24 14:47:56 +0900

    .

--- application/utils/UrlUtils.php
+++ application/utils/UrlUtils.php
@@ -1,22 +1,27 @@
 <?php
-    function getUrlPaths() {
-        $getUrl = '';
-        if (isset($_GET['url'])) {
-            $getUrl = rtrim($_GET['url'], '/');
-            $getUrl = filter_var($getUrl, FILTER_SANITIZE_URL);
-        }
-        return explode('/', $getUrl);
-    }
 
-    function getMethod() {
-        $headers = getallheaders();
-        return $headers['Accept'];
-    }
+function getUrl()
+{
+    return isset($_GET['url']) ? rtrim($_GET['url'], '/') : '';
+}
+function getUrlPaths()
+{
+    $getUrl = getUrl();
+    return $getUrl !== '' ? explode('/', $getUrl) : '';
+}
 
-    function isGetOne() {
-        $urlPaths = getUrlPaths();
-        if(isset($urlPaths[2])) { //one
-            return $urlPaths[2];
-        }
-        return false;
+function getMethod()
+{
+    $headers = getallheaders();
+    return $headers['Accept'];
+}
+
+function isGetOne()
+{
+    $urlPaths = getUrlPaths();
+    if (isset($urlPaths[2])) {
+        //one
+        return $urlPaths[2];
     }
+    return false;
+}
